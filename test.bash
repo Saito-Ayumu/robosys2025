d194139681125005a88efc1bd04ea33092d63e06
Ayumu Saito <saito.ayumu1002@icoud.com>
2025-11-06 12:36:06 +0900

    Add a comand

---
+++ test.bash
@@ -0,0 +1,23 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Ayumu Saito
+
+ng () {
+        echo ${1}行目が違うよ
+        res=1
+}	
+
+res=0
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "$?{out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit $res
