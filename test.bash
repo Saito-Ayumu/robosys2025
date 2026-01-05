35c0ac5ba368cf85496fc0a4f2063fce57792b88
Ayumu Saito <saito.ayumu1002@icoud.com>
2026-01-05 14:12:53 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,25 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2025 Ayumu Saito
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-        echo ${1}行目が違うよ
-        res=1
-}	
+  echo "${1}行目が違うよ" >&2
+  res=1
+}
 
 res=0
 
-### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]       || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+### NORMAL INPUT (min-max) ###
+out=$(seq 5 | ./numscale)
+expected=$'0.000\n0.250\n0.500\n0.750\n1.000'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+### ZSCORE (0 and 2 -> mean=1, std=1 so -1 and 1 exactly) ###
+out=$(printf "0\n2\n" | ./numscale -z)
+expected=$'-1.000\n1.000'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+### STRANGE INPUT (non-number) ###
+out=$(printf "1\nあ\n2\n" | ./numscale 2>/dev/null)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ]  || ng "$LINENO"
+
+### EMPTY (no numbers) ###
+out=$(echo | ./numscale 2>/dev/null)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ]  || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res
+
