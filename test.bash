edd9a408809b9a592e2756036ebe68073c1a9a3e
Ayumu Saito <saito.ayumu1002@icoud.com>
2026-01-06 14:04:12 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -29,6 +29,21 @@ out=$(echo | ./numscale 2>/dev/null)
 [ "$?" = 1 ]       || ng "$LINENO"
 [ "${out}" = "" ]  || ng "$LINENO"
 
+### IGNORE COMMENTS ###
+out=$(printf "10\n# comment\n\n20\n30\n" | ./numscale)
+expected=$'0.000\n0.500\n1.000'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+### PRECISION OPTION ###
+out=$(seq 3 | ./numscale -p 1)
+expected=$'0.0\n0.5\n1.0'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+### ERROR: SAME VALUES (min == max) ###
+out=$(yes 10 | head -n 3 | ./numscale 2>/dev/null)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ]  || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
 
